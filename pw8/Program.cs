72407758224db43279c5437c6788d62271933be4
tweeki99 <danil.shipilov.99@gmail.com>
2019-01-15 19:55:21 +0600

    pw8

---
+++ pw8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray test = new RangeOfArray(-7,5);
+
+            test.InitArray();
+            //test.ShowArray();
+
+            Console.WriteLine(test[0]);
+        }
+    }
+}
---
+++ pw8/RangeOfArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw8
+{
+    class RangeOfArray
+    {
+        private int _upperIndex;
+        private int _lowerIndex;
+        private int[] _array;
+
+        public int this[int index]
+        {
+            get
+            {
+                return _array[index - _lowerIndex];
+            }
+            set
+            {
+                _array[index - _lowerIndex] = value;
+            }
+        }
+
+        public RangeOfArray(int lowerIndex, int upperIndex)
+        {
+            _upperIndex = upperIndex;
+            _lowerIndex = lowerIndex;
+            _array = new int[_upperIndex - _lowerIndex + 1];          
+        }
+
+        public void InitArray()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = i;
+            }
+        }
+
+        public void ShowArray()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.Write( _array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
